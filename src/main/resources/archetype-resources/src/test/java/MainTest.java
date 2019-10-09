9518d3aeddc715e5b88653caa0b0f509b4370bee
Alexander Soto <alexsotocx@AlexandHouseMBP.fritz.box>
2019-10-09 22:01:42 +0200

    initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+package ${groupId};
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+      assertEquals(6, 12 / 2);
+    }
+}
